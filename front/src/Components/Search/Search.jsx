9ea227db3bad1548960ad048d41c6ad8310ea761
YepesF <yepilef@gmail.com>
2022-08-24 18:56:47 -0500

    Se agrega el componente SearchBar, se hace provicional un elemento para la posible prediccion de resultados.

--- front/src/Components/Search/Search.jsx
+++ front/src/Components/Search/Search.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-import {RiSearch2Line} from "react-icons/ri"
-
+import { RiSearch2Line } from "react-icons/ri";
 
 export default function Search() {
-    
   return (
-  <div>
+    <div>
       <form>
-      <input
-        className="input" 
-        type="text"
-        placeholder="Busca un Libro..."
-        // value=
-        // onChange=
-      />
-      <button title="Search" type="submit"><RiSearch2Line size="1.5rem" /></button>
+        <input
+          className="input"
+          type="text"
+          placeholder="Busca un Libro..."
+          // value=
+          // onChange=
+        />
+        <button title="Search" type="submit">
+          <RiSearch2Line size="1.5rem" />
+        </button>
       </form>
-   
-    {/* Esto es provisorio para que ocupe el lugar en el navBar */}
-  </div>
- );
+
+      {/* Esto es provisorio para que ocupe el lugar en el navBar */}
+    </div>
+  );
 }
--- front/src/Components/SearchBar/SearchBar.jsx
+++ front/src/Components/SearchBar/SearchBar.jsx
@@ -1,41 +1,30 @@
-import React, { useState } from "react";
-
-const obj = [
-  { id: 1, name: "abc" },
-  { id: 2, name: "bcd" },
-  { id: 3, name: "cde" },
-  { id: 4, name: "def" },
-  { id: 5, name: "efg" },
-  { id: 6, name: "fgh" },
-];
+import React from "react";
 
 export default function SerachBar() {
-  const [resultsSearch, setResultsSearch] = useState([]);
-  const handledSubmit = (e) => {
-    e.preventDefault();
+  const handledSubmit = (event) => {
+    event.preventDefault();
   };
 
-  const handleChange = (e) => {
-    if (e.target.value) {
-      let results = obj.filter((o) => {
-        return o.name.includes(e.target.value);
-      });
-      if (results.length) {
-        setResultsSearch(results);
-      }
-    } else {
-      setResultsSearch([]);
-    }
+  const handleChange = (event) => {
+    event.preventDefault();
   };
+
   return (
     <>
       <form onSubmit={handledSubmit}>
-        <input type={"text"} onChange={handleChange} />
+        <input
+          type={"text"}
+          className="input"
+          placeholder="Busca un Libro..."
+          onChange={handleChange}
+        />
+        {/* 
+        // Se va a utilizar para la prediccion de busqueda de ser necesario
         <ul>
           {resultsSearch.length
             ? resultsSearch.map((r) => <li>{r.name}</li>)
             : null}
-        </ul>
+        </ul> */}
         <input type={"submit"} value={"Search"} />
       </form>
     </>
