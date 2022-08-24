55c3ca17ab2d0ca25501fed8e616a2add7adf22c
Flori O <floroldani@hotmail.com>
2022-08-24 14:49:07 -0300

    Crear LandingPage

--- front/src/Components/LandingPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LandingPage() {
-  return <div>Hola Estoy en la LandingPage</div>;
-}
---
+++ front/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//COMPONENTES
+import NavBar from "../NavBar/NavBar.jsx"
+import NavBar2 from "../NavBar2/NavBar2.jsx"
+
+
+
+export default function LandingPage() {
+
+  return (
+  <div>
+    <NavBar />
+    <NavBar2 />
+  </div>
+  )
+}
