cf18ff5f59ebc00518b63264548e606364cf80be
YepesF <yepilef@gmail.com>
2022-08-26 20:36:30 -0500

    Se obtienen todas las categorias desde el back para mostrarlas en el front

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -4,76 +4,80 @@ dotenv.config();
 
 const baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
-
-
-
-
 export const GET_ALL_BOOKS = "GET_ALL_BOOKS";
 export const GET_NAME_BOOKS = "GET_NAME_BOOKS";
 export const GET_BOOKS_ID = "GET_BOOKS_ID";
 export const DELETE_BOOKS_DETAIL = "DELETE_BOOKS_DETAIL";
-
-
-
-
+export const GET_ALL_CATEGORIES = "GET_ALL_CATEGORIES";
 
 export function getAllBooks() {
-    return function (dispatch) {
-     
-      axios.get(`/catalog`)
-        .then(response => {
-          dispatch({
-            type: "GET_ALL_BOOKS",
-            payload: response.data
-          });
-        })
-      .catch(error => {
-        console.log("getAllBooks",error)
+  return function (dispatch) {
+    axios
+      .get(`/catalog`)
+      .then((response) => {
+        dispatch({
+          type: "GET_ALL_BOOKS",
+          payload: response.data,
+        });
       })
-    };
-  }
-
-
+      .catch((error) => {
+        console.log("getAllBooks", error);
+      });
+  };
+}
 
 export function getNameBooks(title) {
-    return function (dispatch) {
-     
-      axios.get(`/catalog?title=${title}`)
-        .then(response => {
-          dispatch({
-            type: "GET_NAME_BOOKS",
-            payload: response.data
-          });
-        })
-      .catch(error => {
-        console.log("getNameBooks",error)
+  return function (dispatch) {
+    axios
+      .get(`/catalog?title=${title}`)
+      .then((response) => {
+        dispatch({
+          type: "GET_NAME_BOOKS",
+          payload: response.data,
+        });
       })
-    };
-  }
+      .catch((error) => {
+        console.log("getNameBooks", error);
+      });
+  };
+}
 
-
-
-
-  export function getBooksId(id) {
-    return function (dispatch) {
-
-      axios.get(`/catalog/${id}`)
-        .then(response => {
-          dispatch({
-            type: "GET_BOOKS_ID",
-            payload: response.data
-          });
-        })
-      .catch(error => {
-        console.log("getBooksId",error)
+export function getBooksId(id) {
+  return function (dispatch) {
+    axios
+      .get(`/catalog/${id}`)
+      .then((response) => {
+        dispatch({
+          type: "GET_BOOKS_ID",
+          payload: response.data,
+        });
       })
-    };
-  }
+      .catch((error) => {
+        console.log("getBooksId", error);
+      });
+  };
+}
 
+export function deleteBookDetail(id) {
+  //--> Lo utilizo para desmontar el componente de detalle
+  return {
+    type: "DELETE_BOOKS_DETAIL",
+    payload: id,
+  };
+}
 
-  export function deleteBookDetail(id) {  //--> Lo utilizo para desmontar el componente de detalle
-    return {
-      type: "DELETE_BOOKS_DETAIL",
-      payload: id
-    };
-  }
+export function getCategories() {
+  return function (dispatch) {
+    axios
+      .get(`/categories`)
+      .then((response) => {
+        dispatch({
+          type: GET_ALL_CATEGORIES,
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log("getCategories", error);
+      });
+  };
+}
--- front/src/reducer/index.js
+++ front/src/reducer/index.js
@@ -1,49 +1,52 @@
-import { GET_ALL_BOOKS, GET_NAME_BOOKS, GET_BOOKS_ID, DELETE_BOOKS_DETAIL } from "../actions/index";
+import {
+  GET_ALL_BOOKS,
+  GET_NAME_BOOKS,
+  GET_BOOKS_ID,
+  DELETE_BOOKS_DETAIL,
+  GET_ALL_CATEGORIES,
+} from "../actions/index";
 
 const initialState = {
-    allBooks: [],
-    bookDetail: []
+  allBooks: [],
+  bookDetail: [],
+  categories: [],
 };
 
 function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_BOOKS:
+      return {
+        ...state,
+        allBooks: action.payload,
+      };
 
+    case GET_NAME_BOOKS:
+      return {
+        ...state,
+        allBooks: action.payload,
+      };
 
+    case GET_BOOKS_ID:
+      return {
+        ...state,
+        bookDetail: action.payload,
+      };
 
+    case DELETE_BOOKS_DETAIL: //Limpia el componente --> useEffect
+      return {
+        ...state,
+        bookDetail: [],
+      };
 
+    case GET_ALL_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload,
+      };
 
-    switch (action.type) {
-
-        case GET_ALL_BOOKS:
-         
-            return {
-                ...state,
-                allBooks: action.payload
-            }
-
-        case GET_NAME_BOOKS:
-
-            return {
-                ...state,
-                allBooks: action.payload
-            }
-        
-        case GET_BOOKS_ID:
-            
-           return{
-                ...state,
-                bookDetail: action.payload
-           }
-
-        case DELETE_BOOKS_DETAIL:   //Limpia el componente --> useEffect
-           return {
-               ...state,
-               bookDetail: []
-           }
-
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
