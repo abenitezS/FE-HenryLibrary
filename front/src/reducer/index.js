22702aadbd366f15b5340107c4803d0cca1f890e
Pablo Szejpiacki <molder26@hotmail.com>
2022-08-28 12:41:44 -0300

    fix button delete details

--- front/src/reducer/index.js
+++ front/src/reducer/index.js
@@ -1,102 +1,104 @@
 import {
-  GET_ALL_BOOKS,
-  GET_NAME_BOOKS,
-  GET_BOOKS_ID,
-  DELETE_BOOKS_DETAIL,
-  GET_ALL_CATEGORIES,
-  GET_ALL_BOOKS_BY_CATEGORY,
-  POST_BOOK,
-  SET_PAGE,
-  GET_BOOKS_COUNT,
-  BANNED_BOOK,
-  DELETE_LOGICO_BOOK,
-  GET_ALL_BOOKS_BY_AUTHOR,
+    GET_ALL_BOOKS,
+    GET_NAME_BOOKS,
+    GET_BOOKS_ID,
+    DELETE_BOOKS_DETAIL,
+    GET_ALL_CATEGORIES,
+    GET_ALL_BOOKS_BY_CATEGORY,
+    POST_BOOK,
+    SET_PAGE,
+    GET_BOOKS_COUNT,
+    BANNED_BOOK,
+    DELETE_LOGICO_BOOK,
+    GET_ALL_BOOKS_BY_AUTHOR,
 } from "../actions/index";
 
 const initialState = {
-  allBooks: [],
-  bookDetail: [],
-  categories: [],
-  msg: [],
-  page: 0,
-  cantBooks: 0,
+    allBooks: [],
+    bookDetail: [],
+    categories: [],
+    msg: [],
+    page: 0,
+    cantBooks: 0,
 };
 
 function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case GET_ALL_BOOKS:
-      return {
-        ...state,
-        allBooks: action.payload,
-      };
+    switch (action.type) {
+        case GET_ALL_BOOKS:
+            return {
+                ...state,
+                allBooks: action.payload,
+            };
 
-    case GET_NAME_BOOKS:
-      return {
-        ...state,
-        allBooks: action.payload,
-      };
+        case GET_NAME_BOOKS:
+            return {
+                ...state,
+                allBooks: action.payload,
+            };
 
-    case GET_BOOKS_ID:
-      return {
-        ...state,
-        bookDetail: action.payload,
-      };
+        case GET_BOOKS_ID:
+            return {
+                ...state,
+                bookDetail: action.payload,
+            };
 
-    case DELETE_BOOKS_DETAIL: //Limpia el componente --> useEffect
-      return {
-        ...state,
-        bookDetail: [],
-      };
+        case DELETE_BOOKS_DETAIL: //Limpia el componente --> useEffect
+            return {
+                ...state,
+                bookDetail: [],
+            };
 
-    case GET_ALL_CATEGORIES:
-      return {
-        ...state,
-        categories: action.payload,
-      };
+        case GET_ALL_CATEGORIES:
+            return {
+                ...state,
+                categories: action.payload,
+            };
 
-    case GET_ALL_BOOKS_BY_CATEGORY:
-      return {
-        ...state,
-        allBooks: action.payload,
-      };
+        case GET_ALL_BOOKS_BY_CATEGORY:
+            return {
+                ...state,
+                allBooks: action.payload,
+            };
 
-    case POST_BOOK:
-      return {
-        ...state,
-        msg: [...action.payload],
-      };
+        case POST_BOOK:
+            return {
+                ...state,
+                msg: [...action.payload],
+            };
 
-    case SET_PAGE:
-      return {
-        ...state,
-        page: action.payload,
-      };
+        case SET_PAGE:
+            return {
+                ...state,
+                page: action.payload,
+            };
 
-    case GET_BOOKS_COUNT:
-      return {
-        ...state,
-        cantBooks: action.payload,
-      };
+        case GET_BOOKS_COUNT:
+            return {
+                ...state,
+                cantBooks: action.payload,
+            };
 
-    case BANNED_BOOK:
-      return {
-        ...state,
-      };
+        case BANNED_BOOK:
+            return {
+                ...state,
+            };
 
-    case DELETE_LOGICO_BOOK:
-      return {
-        ...state,
-      };
+        case DELETE_LOGICO_BOOK:
+            return {
+                ...state,
+                allBooks: [...state.allBooks, action.payload],
+                bookDetail: action.payload,
+            };
 
-    case GET_ALL_BOOKS_BY_AUTHOR:
-      return {
-        ...state,
-        allBooks: action.payload,
-      };
+        case GET_ALL_BOOKS_BY_AUTHOR:
+            return {
+                ...state,
+                allBooks: action.payload,
+            };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 }
 
 export default rootReducer;
