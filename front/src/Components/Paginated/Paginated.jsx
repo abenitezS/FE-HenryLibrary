e9462131871fe00365a3bb402ea1cb90e871c1df
Pablo Szejpiacki <molder26@hotmail.com>
2022-08-28 00:08:52 -0300

    Finish paginated frontend

--- front/src/Components/Paginated/Paginated.jsx
+++ front/src/Components/Paginated/Paginated.jsx
@@ -1,14 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getBooksCount } from '../../actions';
+import { getAllBooks, getBooksCount, setPage } from "../../actions";
+
+//CSS
+import styles from "./Paginated.module.css";
 
 export default function Paginated() {
     const dispatch = useDispatch();
-    const cantBooks = useSelector((state) => state.cantBooks);
+    const { cantBooks, page: actualPage } = useSelector((state) => state);
+    const itemsPorPagina = 10;
+    const cantPaginas = Math.ceil(cantBooks / itemsPorPagina) - 1;
+
     useEffect(() => {
         dispatch(getBooksCount());
-      }, [dispatch, cantBooks]);
-  return (
-    <div>Paginado {cantBooks}</div>
-  )
+    }, [dispatch, cantBooks]);
+
+    function handleChangePage(page) {
+        dispatch(setPage(page));
+        dispatch(getAllBooks(page));
+    }
+
+    return (
+        <div>
+            <button
+                disabled={actualPage === 0}
+                className={actualPage > 0 ? styles.Btn : styles.BtnDisabled}
+                key={"prev"}
+                onClick={() => handleChangePage(actualPage - 1)}
+            >
+                {"<"}
+            </button>
+
+            <button
+                disabled={actualPage === cantPaginas}
+                className={
+                    actualPage < cantPaginas ? styles.Btn : styles.BtnDisabled
+                }
+                key={"next"}
+                onClick={() => handleChangePage(actualPage + 1)}
+            >
+                {">"}
+            </button>
+        </div>
+    );
 }
