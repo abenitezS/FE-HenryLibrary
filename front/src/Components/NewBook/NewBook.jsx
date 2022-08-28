88fe025fda3eb18dcdd4ffb3f74bf81e2307d0fa
´Gabriel <Gabrielmarzioli@hotmail.com>
2022-08-28 17:47:02 -0300

    realizando validaciones Formu Creacion

--- front/src/Components/NewBook/NewBook.jsx
+++ front/src/Components/NewBook/NewBook.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { uploadBook } from '../../actions';
-
+import { uploadBook } from "../../actions";
 
 //CSS
 import styles from "./NewBook.module.css";
@@ -11,35 +10,42 @@ export default function NewRecipe() {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const [publisherIdError, setPublisherIdError] = useState(false);
   const [titleError, setTitleError] = useState(false);
   const [descriptionError, setDescriptionError] = useState(false);
   const [priceError, setPriceError] = useState(false);
   const [imageError, setImageError] = useState(false);
   const [publishedDateError, setPublishedDateError] = useState(false);
   const [pageCountError, setPageCountError] = useState(false);
+  const [currentStockError, setcurrentStockError] = useState(false);
   const [languageError, setLanguageError] = useState(false);
-  const [ratingError, setRatingError] = useState(false);
+  const [isBannedError, setIsBannedError] = useState(false);
 
   let [book, setBook] = useState({
+    publisherId: 0,
     title: "",
     description: "",
     price: 0,
     image: "",
     publishedDate: "",
     pageCount: 0,
+    currentStock: 0,
     language: "",
-    rating: 0,
+    isBanned: false
   });
 
   const handleInputsChange = (e) => {
+    setPublisherIdError(false);
     setTitleError(false);
     setDescriptionError(false);
     setPriceError(false);
     setImageError(false);
     setPublishedDateError(false);
     setPageCountError(false);
+    setcurrentStockError(false);
     setLanguageError(false);
-    setRatingError(false);
+    setIsBannedError(false);
+  
 
     setBook({
       ...book,
@@ -47,49 +53,60 @@ export default function NewRecipe() {
     });
   };
 
-  const patternText = new RegExp("^[A-Z]+$", "i");
   const patternURL = new RegExp(
     /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi
   );
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    setPublisherIdError(false);
     setTitleError(false);
     setDescriptionError(false);
     setPriceError(false);
     setImageError(false);
     setPublishedDateError(false);
     setPageCountError(false);
+    setcurrentStockError(false);
     setLanguageError(false);
-    setRatingError(false);
+    setIsBannedError(false);
 
-    if (!book.title || !patternText.test(book.title)) {
-      return setTitleError("enter a title correct for your book");
+    // if (!book.publisherId) {
+    //   return setTitleError("Ya existe ID Cargada");
+    // } ---> validar si no existe en BBDD
+
+    if (!book.title) {
+      return setTitleError("Ingrese un Titulo correcto para su Libro");
     }
     if (!book.description) {
-      return setDescriptionError("enter a description correct for your book");
+      return setDescriptionError("Ingrese una Descripcion para su Libro");
     }
     if (!book.price || !book.price === Number) {
-      return setPriceError("enter a price correct for your book");
-    }
-  
-    if (!patternURL.test(book.image)) {
-      return setImageError("enter format url image");
+      return setPriceError("Ingrese un Precio correcto para su Libro");
     }
 
-    dispatch(uploadBook(book)); // Generar la funcion en Action
+    if (!patternURL.test(book.image)) {
+      return setImageError("Ingrese un formato correcro URL de imagen");
+    }
+
+    if (!book.currentStock || !book.currentStock === Number) {
+      return setcurrentStockError("Ingrese un Stock correcto");
+    }
+
+    dispatch(uploadBook(book));
 
     setBook({
+      publisherId: 0,
       title: "",
       description: "",
       price: 0,
       image: "",
       publishedDate: "",
       pageCount: 0,
+      currentStock: 0,
       language: "",
-      rating: 0,
+      isBanned: false
     });
-    alert("Book created successfully!");
+    alert("Libro creado Exitosamente!");
   };
 
   const handleBackSubmit = (e) => {
@@ -99,60 +116,93 @@ export default function NewRecipe() {
 
   return (
     <div className={styles.containerFormu}>
-      <h1 className={styles.titleFormu}>New Book</h1>
+      <h1 className={styles.titleFormu}>Nuevo Libro</h1>
       <form action="POST">
         <div className={styles.containerInputs}>
+
           <div className={styles.containerInput}>
-            <label>Title: </label>
+            <label>Id de Editorial: </label>
             <input
-              placeholder="insert book title"
+              placeholder="ingrese ID"
+              type="text"
+              name="publisherId"
+              value={book.publisherId}
+              className={styles.inputs}
+              onChange={handleInputsChange}
+            />
+            *Campo Requerido
+            <div className={styles.danger}>
+              {publisherIdError && <p>{publisherIdError}</p>}
+            </div>
+          </div>
+
+          <div className={styles.containerInput}>
+            <label>Titulo: </label>
+            <input
+              placeholder="ingrese el titulo del Libro..."
               type="text"
               name="title"
               value={book.title}
               className={styles.inputs}
               onChange={handleInputsChange}
             />
-            *Required field
+            *Campo Requerido
             <div className={styles.danger}>
               {titleError && <p>{titleError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-
-
-            <label>Description: </label>
+            <label>Descripcion: </label>
             <textarea
-              placeholder="insert book description"
+              placeholder="ingrese descripcion del Libro..."
               type="text"
               name="description"
               value={book.description}
               className={styles.inputs}
               onChange={handleInputsChange}
             />
-            *Required field
+            *Campo Requerido
             <div className={styles.danger}>
               {descriptionError && <p>{descriptionError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-
-
-            <label>price: </label>
+            <label>Precio: </label>
             <input
-              placeholder="insert book price"
+              placeholder="ingrese Precio del Libro..."
               type="number"
               name="price"
               value={book.price}
               className={styles.inputs}
               onChange={handleInputsChange}
             />
-            *Required field
+            *Campo Requerido
             <div className={styles.danger}>
               {priceError && <p>{priceError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-            <label>image: </label>
+            <label>Stock Actual: </label>
+            <input
+              placeholder="ingrese Stock actual..."
+              type="number"
+              name="currentStock"
+              value={book.currentStock}
+              className={styles.inputs}
+              onChange={handleInputsChange}
+            />
+            *Campo Requerido
+            <div className={styles.danger}>
+              {currentStockError && <p>{currentStockError}</p>}
+            </div>
+          </div>
+
+
+          <div className={styles.containerInput}>
+            <label>Imagen: </label>
             <input
               placeholder="URL ej: http://..."
               type="text"
@@ -165,10 +215,11 @@ export default function NewRecipe() {
               {imageError && <p>{imageError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-            <label>Published Date: </label>
+            <label>Fecha de publicacion: </label>
             <input
-              placeholder="insert book Published Date"
+              placeholder="Ingrese Fecha..."
               type="date"
               name="publishedDate"
               value={book.publishedDate}
@@ -179,10 +230,11 @@ export default function NewRecipe() {
               {publishedDateError && <p>{publishedDateError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-            <label>Page Count: </label>
+            <label>Cant. de Paginas:</label>
             <input
-              placeholder="insert book Page Count"
+              placeholder="Ingrese Cant. Paginas del Libro..."
               type="number"
               name="pageCount"
               value={book.pageCount}
@@ -193,10 +245,10 @@ export default function NewRecipe() {
               {pageCountError && <p>{pageCountError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-            <label>language: </label>
+            <label>Lenguaje: </label>
             <input
-              placeholder="insert book language"
               type="text"
               name="language"
               value={book.language}
@@ -207,25 +259,30 @@ export default function NewRecipe() {
               {languageError && <p>{languageError}</p>}
             </div>
           </div>
+
           <div className={styles.containerInput}>
-            <label>rating: </label>
+            <label>Venta: </label>
             <input
-              placeholder="insert book rating"
-              type="number"
-              name="rating"
-              value={book.rating}
+              type="text"
+              name="isBanned"
+              value={book.isBanned}
               className={styles.inputs}
               onChange={handleInputsChange}
             />
             <div className={styles.danger}>
-              {ratingError && <p>{ratingError}</p>}
+              {isBannedError && <p>{isBannedError}</p>}
             </div>
           </div>
+
           <div className={styles.containerButtons}>
-            <button type="submit" onClick={handleOnSubmit} className={styles.button}>
-              Send
+            <button
+              type="submit"
+              onClick={handleOnSubmit}
+              className={styles.button}>Enviar
             </button>
-            <button onClick={handleBackSubmit} className={styles.button}>Cancel</button>
+            <button onClick={handleBackSubmit} className={styles.button}>
+              Cancelar
+            </button>
           </div>
         </div>
       </form>
