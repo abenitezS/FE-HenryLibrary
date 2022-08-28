afb392c67eccff044278466cb81810f47fa4f1f0
Pablo Szejpiacki <molder26@hotmail.com>
2022-08-28 19:08:55 -0300

    Add states for auth in reducer

--- front/src/reducer/index.js
+++ front/src/reducer/index.js
@@ -1,27 +1,33 @@
 import {
-  GET_ALL_BOOKS,
-  GET_NAME_BOOKS,
-  GET_BOOKS_ID,
-  DELETE_BOOKS_DETAIL,
-  GET_ALL_CATEGORIES,
-  GET_ALL_BOOKS_BY_CATEGORY,
-  POST_BOOK,
-  SET_PAGE,
-  GET_BOOKS_COUNT,
-  BANNED_BOOK,
-  DELETE_LOGICO_BOOK,
-  SET_ALL_BOOKS_BY_AUTHOR,
-  SET_AUTHOR_BY_NAME,
+    GET_ALL_BOOKS,
+    GET_NAME_BOOKS,
+    GET_BOOKS_ID,
+    DELETE_BOOKS_DETAIL,
+    GET_ALL_CATEGORIES,
+    GET_ALL_BOOKS_BY_CATEGORY,
+    POST_BOOK,
+    SET_PAGE,
+    GET_BOOKS_COUNT,
+    BANNED_BOOK,
+    DELETE_LOGICO_BOOK,
+    SET_ALL_BOOKS_BY_AUTHOR,
+    SET_AUTHOR_BY_NAME,
 } from "../actions/index";
 
 const initialState = {
-  allBooks: [],
-  bookDetail: [],
-  categories: [],
-  authors: [],
-  msg: [],
-  page: 0,
-  cantBooks: 0,
+    allBooks: [],
+    bookDetail: [],
+    categories: [],
+    authors: [],
+    msg: [],
+    page: 0,
+    cantBooks: 0,
+    status: "checking",
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
 };
 
 function rootReducer(state = initialState, action) {
@@ -92,21 +98,21 @@ function rootReducer(state = initialState, action) {
                 bookDetail: action.payload,
             };
 
-    case SET_ALL_BOOKS_BY_AUTHOR:
-      return {
-        ...state,
-        allBooks: action.payload,
-      };
+        case SET_ALL_BOOKS_BY_AUTHOR:
+            return {
+                ...state,
+                allBooks: action.payload,
+            };
 
-    case SET_AUTHOR_BY_NAME:
-      return {
-        ...state,
-        authors: action.payload,
-      };
+        case SET_AUTHOR_BY_NAME:
+            return {
+                ...state,
+                authors: action.payload,
+            };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 }
 
 export default rootReducer;
